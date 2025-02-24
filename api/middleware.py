6368b260bfed12f4e6d594503596eec0d7d2ada1
Stefano Lacorazza <s.lacorazza@ofiservices.com>
2025-02-24 11:23:29 -0500

    update

---
+++ api/middleware.py
@@ -0,0 +1,11 @@
+class CorsMiddleware:
+    """
+    Middleware to add Access-Control-Allow-Origin header to responses.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response["Access-Control-Allow-Origin"] = "http://127.0.0.1:8000"
+        return response
--- ofi_dashboard_backend/settings.py
+++ ofi_dashboard_backend/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ofi_dashboard_backend.urls'
